02358154ebd337770a2aae35e3ec77adc64c00b2
sylvian <sylvian31@gmail.com>
2019-04-24 19:35:00 -0400

    add HOC auth

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../style.css';
 import Home from './home'
 import Ressources from './ressources'
 import { Route, Switch } from 'react-router-dom'
+import RequireAuth from '../helpers/requireAuth'
 
 export default function App() {
   return (
@@ -11,7 +12,7 @@ export default function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/ressources" component={Ressources} />
+        <Route exact path="/ressources" component={RequireAuth(Ressources)} />
       </Switch>
     </div>
   );
---
+++ src/helpers/requireAuth.jsx
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+export default function (ChildComponent) {
+    class RequireAuth extends Component {
+
+        constructor(props) {
+            super(props);
+            if (!this.props.isLogged) {
+                this.redirect();
+            }
+        }
+
+        // static getDerivedStateFromProps(props){
+        //     if (!this.props.isLogged) {
+        //         this.redirect();
+        //     }
+        // }
+
+        getSnapshotBeforeUpdate(prevProps, prevState){
+            if (!this.props.isLogged) {
+                this.redirect();
+            }
+            return {};
+        }
+
+
+        render() {
+            return <ChildComponent />
+        }
+
+        componentDidUpdate(){
+
+        }
+
+        redirect = () => {
+            this.props.history.push("/");
+        }
+
+    }
+    const mapStateToProps = (state) => ({
+        isLogged: state.authReducer.isLogged
+    });
+
+    return connect(mapStateToProps)(RequireAuth);
+}
